4eced16e5fb15ecff57f60de97d1ff5099210149
Nikolai Burnyshev <burnyshevni@gmail.com>
2022-07-16 16:51:03 +0500

    Changing animals added.

--- src/main/java/com/boot/demo/controllers/AnimalController.java
+++ src/main/java/com/boot/demo/controllers/AnimalController.java
@@ -6,8 +6,11 @@ import com.boot.demo.repos.AnimalRepository;
 import com.boot.demo.repos.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Objects;
 
 @Controller
@@ -30,9 +33,29 @@ public class AnimalController {
         return "animal";
     }
 
+    @GetMapping("/patch/{id}")
+    public String goToChange(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("animal", animalRepository.findById(id).get());
+        return "changeAnimal";
+    }
+
+    @PatchMapping("/{id}")
+    public String changeAnimal(@PathVariable("id") Long id, @Valid @ModelAttribute("animal") Animal animal, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "changeAnimal";
+        }
+        Animal old = animalRepository.findById(id).get();
+        old.setName(animal.getName());
+        old.setBirth(animal.getBirth());
+        old.setSex(animal.getSex());
+        animalRepository.save(old);
+        model.addAttribute("animal", animal);
+        model.addAttribute("person", PersonController.currentUser);
+        return "animal";
+    }
+
     @DeleteMapping("/{id}")
     public String deleteAnimal(@PathVariable("id") Long id, Model model) {
-        System.out.println(id);
         animalRepository.delete(animalRepository.findById(id).get());
         return "redirect:/person/" + PersonController.currentUser.getId();
     }
