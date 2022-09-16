b1d1ba3dd280e0d36b9aaf42d03a5c76ff436f3f
Nikolai Burnyshev <burnyshevni@gmail.com>
2022-09-16 19:10:47 +0300

    Entrance CSS added.

--- src/main/java/com/boot/demo/config/DataSource.java
+++ src/main/java/com/boot/demo/config/DataSource.java
@@ -2,9 +2,12 @@ package com.boot.demo.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.annotation.PreDestroy;
 
 public class DataSource {
     @Bean
--- src/main/java/com/boot/demo/config/WebSecurityConfig.java
+++ src/main/java/com/boot/demo/config/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.annotation.PreDestroy;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
